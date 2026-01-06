532de6c6ce0f806aa4f62ff134db09e8f39ba3ea
Marcelo Lima <m-lima@users.noreply.github.com>
2026-01-06 15:11:20 +0100

    Bump deps

--- src/dns.rs
+++ src/dns.rs
@@ -1,3 +1,33 @@
+enum Endpoint<'a> {
+    NotFound,
+    NoChange,
+    Update(cloudflare::endpoints::dns::dns::UpdateDnsRecord<'a>),
+}
+fn get_endpoint<'a, 'z: 'a>(
+    entries: &'a [cloudflare::endpoints::dns::dns::DnsRecord],
+    zone_identifier: &'z str,
+    ip: std::net::Ipv4Addr,
+) -> Endpoint<'a> {
+    for entry in entries {
+        if let cloudflare::endpoints::dns::dns::DnsContent::A { content } = entry.content {
+            if content == ip {
+                tracing::info!("DNS entry needs no update");
+                return Endpoint::NoChange;
+            }
+            return Endpoint::Update(cloudflare::endpoints::dns::dns::UpdateDnsRecord {
+                zone_identifier,
+                identifier: &entry.id,
+                params: cloudflare::endpoints::dns::dns::UpdateDnsRecordParams {
+                    name: &entry.name,
+                    content: cloudflare::endpoints::dns::dns::DnsContent::A { content: ip },
+                    proxied: Some(false),
+                    ttl: None,
+                },
+            });
+        }
+    }
+    Endpoint::NotFound
+}
 pub async fn update(
     token: String,
     record: String,
@@ -19,61 +49,37 @@ pub async fn update(
         },
     };
 
-    let mut current = client
+    let current = client
         .request(&endpoint)
         .await
         .map_err(Error::Request)?
         .result;
 
-    if let Some(entry) = current.pop() {
-        if !current.is_empty() {
-            return Err(Error::WrongRecordCount(current.len() + 1));
-        }
-
-        let endpoint = cloudflare::endpoints::dns::dns::UpdateDnsRecord {
-            zone_identifier,
-            identifier: &entry.id,
-            params: cloudflare::endpoints::dns::dns::UpdateDnsRecordParams {
-                name: &entry.name,
-                content: {
-                    match entry.content {
-                        cloudflare::endpoints::dns::dns::DnsContent::A { content } => {
-                            if content == ip {
-                                tracing::info!("DNS entry needs no update");
-                                return Ok(());
-                            }
-                            cloudflare::endpoints::dns::dns::DnsContent::A { content: ip }
-                        }
-                        content => return Err(Error::UnexpectedRecordType(content)),
-                    }
+    match get_endpoint(&current, zone_identifier, ip) {
+        Endpoint::NotFound => {
+            let endpoint = cloudflare::endpoints::dns::dns::CreateDnsRecord {
+                zone_identifier,
+                params: cloudflare::endpoints::dns::dns::CreateDnsRecordParams {
+                    name: &record,
+                    content: cloudflare::endpoints::dns::dns::DnsContent::A { content: ip },
+                    proxied: Some(false),
+                    ttl: None,
+                    priority: None,
                 },
-                proxied: Some(false),
-                ttl: None,
-            },
-        };
+            };
 
-        client
+            client
+                .request(&endpoint)
+                .await
+                .map_err(Error::Update)
+                .map(|_| ())
+        }
+        Endpoint::NoChange => Ok(()),
+        Endpoint::Update(endpoint) => client
             .request(&endpoint)
             .await
             .map_err(Error::Update)
-            .map(|_| ())
-    } else {
-        let endpoint = cloudflare::endpoints::dns::dns::CreateDnsRecord {
-            zone_identifier,
-            params: cloudflare::endpoints::dns::dns::CreateDnsRecordParams {
-                name: &record,
-                content: cloudflare::endpoints::dns::dns::DnsContent::A { content: ip },
-                proxied: Some(false),
-                ttl: None,
-                priority: None,
-            },
-        };
-
-        client
-            .request(&endpoint)
-            .await
-            .map_err(Error::Update)
-            .map(|_| ())
+            .map(|_| ()),
     }
 }
 
@@ -85,8 +91,4 @@ pub enum Error {
     Request(cloudflare::framework::response::ApiFailure),
     #[error("Unable to update the DNS record: {0}")]
     Update(cloudflare::framework::response::ApiFailure),
-    #[error("Expected at most a single record, but got {0}")]
-    WrongRecordCount(usize),
-    #[error("Unexpcted content type: {0:?}")]
-    UnexpectedRecordType(cloudflare::endpoints::dns::dns::DnsContent),
 }
